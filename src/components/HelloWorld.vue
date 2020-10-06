dac9d8737af2e3ae46906bd89ba5ea1bd0547dec
chenchuanjun <3022397866@qq.com>
2020-10-06 14:45:19 +0800

    提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <div id="test">无可奈何花落去，似曾相识燕归来</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#test {
+    color: red;
+    width: 750px;
+    height: 200px;
+    font-size: 24px;
+    opacity: 0.8;
+    margin-bottom: 120px;
+  }
+</style>
